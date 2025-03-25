b5241529752aa95bad546ae5dfd38db1b2be86fc
AkimovSerge <akimovsg@gmail.com>
2025-03-25 22:29:03 +0500

    отработан алгоритм расчета числа

--- SF_DATA_SCIENSE/Задание 8.1 Модуль 8 (HW-01)/game_v3.py
+++ SF_DATA_SCIENSE/Задание 8.1 Модуль 8 (HW-01)/game_v3.py
@@ -1,14 +1,28 @@
+import numpy as np
+import math
+count = 0
+
 mu_list = []
 for i in range(1, 101):
-    mu_list.append(i)
-#print(mu_list)
-number = 60
-predict_number = 75
-mid_idx = (predict_number - 1)# находим индекс элемента в списке.
-#if mu_list[mid_idx] > number:
-if predict_number > number:
-    predict_number = np.mean(mu_list[ : mid_idx])
-    print(round(predict_number))
-else:
-    predict_number = np.mean(mu_list[mid_idx: ])
-    print(round(predict_number))
+   mu_list.append(i)
+prediction = (50, 101, 1) # predict_number = 50, up_limiter = 101, low_limiter = 1
+number = 100
+while True:
+    count +=1
+    def fork(predict_number, up_limiter, low_limiter) -> int: 
+        mid_idx = (predict_number - 1)
+        if predict_number > number:
+            up_limiter = predict_number
+            low_idx = low_limiter - 1
+            predict_number = math.ceil(np.mean(mu_list[low_idx : mid_idx]))
+
+        else: # predict_number < number
+            low_limiter = predict_number
+            up_idx = up_limiter - 1
+            predict_number = math.ceil(np.mean(mu_list[mid_idx: up_idx]))
+
+        return predict_number, up_limiter, low_limiter
+    prediction = fork(*prediction)
+    if number == prediction[0]:
+        break  # выход из цикла - Алгоритм расчитал число!
+print(f"Алгоритм расчитал число! Это число = {number}, за {count} циклов.")
